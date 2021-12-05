ca5411476cc7f7fcfabea40320e7af40c3ec1a97
papo-97 <95270166+papo-97@users.noreply.github.com>
2021-12-05 13:49:46 -0800

    simple number

---
+++ SimpleNumber.js
@@ -0,0 +1,11 @@
+function simple(Number) {
+  if (Number % 2 === 0) {
+    return true;
+    console.log("YES");
+  } else {
+    return false;
+    console.log("NO");
+  }
+}
+simple(401);
+simple(63);
