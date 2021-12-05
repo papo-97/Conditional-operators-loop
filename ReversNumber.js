050960551ae2a3487a9fa398010264aca9fc5a5a
papo-97 <95270166+papo-97@users.noreply.github.com>
2021-12-05 13:22:57 -0800

    count the revers  of a number

---
+++ ReversNumber.js
@@ -0,0 +1,11 @@
+function Reverse(num) {
+  let rev = 0;
+  let lastDigit;
+
+  while (num != 0) {
+    lastDigit = num % 10;
+    rev = rev * 10 + lastDigit;
+    num = Math.floor(num / 10);
+  }
+  return rev;
+}
